48a2412911904a4c5682646db8514aa3e386479c
Jaith Darrah <generic.jaithd@gmail.com>
2024-08-30 17:57:41 -0500

    Add Prettier and format files
    - Install Prettier as a dev dependency
    - Format JavaScript and JSON files
    - Update package.json with Prettier script

--- src/commands/utility/test.js
+++ src/commands/utility/test.js
@@ -1,10 +1,10 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('test')
-        .setDescription('Replies with "Hello!"'),
-    async execute(interaction) {
-        await interaction.reply('Hello!');
-    },
+  data: new SlashCommandBuilder()
+    .setName("test")
+    .setDescription('Replies with "Hello!"'),
+  async execute(interaction) {
+    await interaction.reply("Hello!");
+  },
 };
