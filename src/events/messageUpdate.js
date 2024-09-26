19d45a26e8515a8690de813e4495982b5537f989
Jaith Darrah <generic.jaithd@gmail.com>
2024-09-26 01:27:09 -0500

    Add messageUpdate.js to handle edited message logging

---
+++ src/events/messageUpdate.js
@@ -0,0 +1,58 @@
+const { Events, EmbedBuilder, AttachmentBuilder } = require("discord.js");
+const axios = require("axios");
+
+module.exports = {
+  name: Events.MessageUpdate,
+  async execute(oldMessage, newMessage) {
+    const dumpChannelId = "1288746613203931137";
+    const diffLogChannelId = "1288702256895623211";
+
+    if (oldMessage.author.bot || oldMessage.content === newMessage.content) {
+      return;
+    }
+
+    try {
+      const message1 = oldMessage.content;
+      const message2 = newMessage.content;
+
+      const data = {
+        beforeText: message1,
+        afterText: message2,
+      };
+
+      const url = `http://localhost:3000/difference`;
+      const response = await axios.post(url, data);
+
+      const { imageId, imageUrl } = response.data;
+
+      console.log("Image Id:", imageId);
+
+      const attachment = new AttachmentBuilder(imageUrl, {
+        name: `${imageId}.png`,
+      });
+
+      const dumpChannel = await newMessage.guild.channels.fetch(dumpChannelId);
+      const sentMessage = await dumpChannel.send({
+        files: [attachment],
+      });
+
+      const discordImageUrl = sentMessage.attachments.first().url;
+
+      const diffEmbed = new EmbedBuilder()
+        .setColor("ffd166")
+        .setTitle(" ")
+        .setImage(discordImageUrl)
+        .addFields(
+          { name: "Before", value: message1, inline: true },
+          { name: "After", value: message2, inline: true }
+        )
+        .setTimestamp();
+
+      const diffLogChannel =
+        await newMessage.guild.channels.fetch(diffLogChannelId);
+      await diffLogChannel.send({ embeds: [diffEmbed] });
+    } catch (error) {
+      console.error("Error sending image or creating embed:", error);
+    }
+  },
+};
